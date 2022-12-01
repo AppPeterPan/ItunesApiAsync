dc7f5ee0203ed88bca89fcaa6eb4ad4116b7c87e
peter <apppeterpan@gmail.com>
2022-12-01 15:45:51 +0800

    Initial Commit

---
+++ ItunesApiAsync/ItunesApiAsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItunesApiAsyncApp.swift
+//  ItunesApiAsync
+//
+//  Created by Peter Pan on 2022/12/1.
+//
+
+import SwiftUI
+
+@main
+struct ItunesApiAsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
